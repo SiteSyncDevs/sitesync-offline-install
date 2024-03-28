c16681724a7be6e021dbd7ba15feeb929d27862f
Nicholas Gendron <nickgendron28@gmail.com>
2024-03-28 12:03:42 -0500

    added apt offline and aloxy core installer

--- offline-install.sh
+++ offline-install.sh
@@ -7,7 +7,9 @@
   JAVA_INSTALL_DIR="/usr/bin/java"
   FONTCONFIG_DIR="$PROJECT_DIR/fontconfig"
   CHIRPSTACK_DOCKER_IMAGES="$PROJECT_DIR/images" 
-
+  APT_OFFLINE_DIR="$PROJECT_DIR/apt-offline"
+  ALOXY_CORE_DIR="$PROJECT_DIR/aloxycore"
+  
 function installDocker() {
 
   if [ ! -d "$DOCKER_DEB_DIR" ]; then
@@ -51,6 +53,21 @@ function loadChirpstackImages(){
       echo "ERROR: Directory $CHIRPSTACK_DOCKER_IMAGES not found." >&2
     fi
 }
+function loadAloxyCoreImage(){
+  echo "INFO: Loading Aloxy Core Docker image"
+  if [ -d "$ALOXY_CORE_DIR" ]; then
+      for image_file in "$ALOXY_CORE_DIR"/*.tar; do
+        if [ -f "$image_file" ]; then
+          echo "INFO: Loading image from $image_file..."
+            docker load < "$image_file" 1>/dev/null
+        else
+          echo "ERROR: No Aloxy Core Docker images found in installation directory." >&2
+        fi
+      done
+    else
+      echo "ERROR: Directory $ALOXY_CORE_DIR not found." >&2
+    fi
+}
 
 function installJava(){
   echo "INFO: Preparing to install Java"
@@ -72,19 +89,40 @@ function installJava(){
   fi
 }
 
+function installAptOffline(){
+  echo "INFO: Preparing to install APT Offline"
+  if [ ! -d "$APT_OFFLINE_DIR" ]; then
+    echo "ERROR: Directory $APT_OFFLINE_DIR does not exist." >&2
+    exit 1
+  fi
+
+  APT_OFFLINE_DEB=$(find "$APT_OFFLINE_DIR" -type f -name "*.deb")
+
+  if [ -z "$APT_OFFLINE_DEB" ]; then
+    echo "ERROR: No apt-offline installation file found in $APT_OFFLINE_DIR." >&2
+    exit 1
+  else
+      echo "INFO: Found apt-offline installation package: $APT_OFFLINE_DEB"
+      echo "INFO: Installing apt-offline"
+      sudo dpkg -i $APT_OFFLINE_DEB 1>/dev/null
+      echo "INFO: apt-offline installation complete"
+  fi
+}
 
 
 function printUsage() {
-    echo "Usage: $0 [options]"
-    echo "Options:"
-    echo "  -d      Install and load Docker images"
-    echo "  -c      Load Chirpstack Docker images"
-    echo "  -j      Install Java"
-    echo "  -i      Install Ignition"
-    echo "  -h      Display this help message"
-    echo "  --help"
+  echo "Usage: $0 [options]"
+  echo "Options:"
+  echo "  -d      Install and load Docker images"
+  echo "  -c      Load Chirpstack Docker images"
+  echo "  -a      Load Aloxy Core Docker images"
+  echo "  -j      Install Java"
+  echo "  -i      Install Ignition"
+  echo "  -o      Install APT Offline"
+  echo "  -A      Install everything"
+  echo "  -h      Display this help message"
+  echo "  --help  Display this help message"
 }
-
 
 function main(){
  
@@ -112,16 +150,19 @@ function main(){
           exit 1
       fi
   done
+  local installAll=false
 
-
-  while getopts "djich" flag
+  while getopts "djicahA" flag
   do
       case "${flag}" in
           h) printUsage exit 0;;
           d) installDocker;;
           j) installJava;;
-          i) installIgnition=true;;
+          o) loadAloxyImages;;
+          i) installIgnition;;
           c) loadChirpstackImages;;
+          a) installAptOffline;;
+          A) installAll=true;;
           \?) 
               echo "Invalid option: -${OPTARG}" >&2
               printUsage
@@ -135,18 +176,18 @@ function main(){
       esac
   done
 
+  if [[ "$installAll" == "true" ]]; then
+      installDocker
+      installJava
+      installIgnition
+      loadChirpstackImages
+      installAptOffline
+  fi
+
   if [[ "$installDocker" == "true" ]]; then
-    echo "Docker will be installed"
-  fi
-    if [[ "$installJava" == "true" ]]; then
-    echo "Java will be installed"
-  fi
-    if [[ "$installIgnition" == "true" ]]; then
-    echo "Ignition will be installed"
-  fi
-    if [[ "$loadChirpstack" == "true" ]]; then
-    echo "Chirp will be installed"
-  fi
+
+
+  
 
 }
 
